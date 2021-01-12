b4df850aa4897ae8f8153c627622745ae22336e9
Taiga <mokuzuzzz@gmail.com>
2021-01-13 03:11:16 +1000

    feat: add atoms

--- packages/app/recoil/atoms.ts
+++ packages/app/recoil/atoms.ts
@@ -1,11 +1,26 @@
-import { atom } from "recoil";
-import {Message} from "../types"
+import { atom } from 'recoil'
+import Portal from '../components/collab/portal'
+import { Message } from '../types'
 export const messagesState = atom<Message[]>({
-    key: "messagesState",
-    default: []
+  key: 'messagesState',
+  default: [],
 })
 
 export const loggedInUserState = atom<string>({
-    key: "loggedInUserState",
-    default: ""
+  key: 'loggedInUserState',
+  default: '',
 })
+
+export const roomDataState = atom<{
+  id: string
+  key: string
+} | null>({
+  key: 'roomDataState',
+  default: null,
+})
+
+export const portalState = atom<Portal | null>({
+  key: 'portalState',
+  default: null,
+})
+export const senderState = atom<string[]>({ key: 'senderState', default: [] })
