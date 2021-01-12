1263afe40307ccd3946d912fba50d5dd1678308e
Taiga <mokuzuzzz@gmail.com>
2021-01-13 03:13:25 +1000

    feat: add userinput

--- packages/app/containers/user-input.tsx
+++ packages/app/containers/user-input.tsx
@@ -1,42 +1,60 @@
-import {useEffect, useState} from "react" 
-import { useSetRecoilState, useRecoilState } from "recoil"
-import { loggedInUserState, messagesState } from "../recoil/atoms"
-
-export const UserInput = () => {
-    const [inputRef, setInputRef] =  useState(null)
-    const [text, changeText] = useState("")
-    const setMessages = useSetRecoilState(messagesState)
-    const [loggedInUser] = useRecoilState(loggedInUserState) 
-    const clearnUpInput = () => {
-        changeText("")
+import { useEffect, useRef, useState } from 'react'
+import { useSetRecoilState, useRecoilState, useRecoilValue } from 'recoil'
+import { loggedInUserState, messagesState, portalState } from '../recoil/atoms'
+import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
+import Portal from '../components/collab/portal'
+export const UserInput = ({ portal }: { portal: Portal }) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [text, changeText] = useState('')
+  const setMessages = useSetRecoilState(messagesState)
+  const loggedInUser = useRecoilValue(loggedInUserState)
+  const clearnUpInput = () => {
+    changeText('')
+  }
+  const updateMessgages = (text) => {
+    setMessages((curVal) => {
+      console.log(`text is ${text}`)
+      return [...curVal, { sender: loggedInUser, text }]
+    })
+  }
+  useEffect(() => {
+    if (!inputRef.current) return
+    const onEnterPressed = (e) => {
+      if (e.keyCode === 13) {
+        e.preventDefault()
+        portal.emitSendMessage(loggedInUser, e.target.value)
+        updateMessgages(e.target.defaultValue)
+        clearnUpInput()
+      }
     }
-    const updateMessgages = (text) => {
-        setMessages((curVal) => {
-            console.log(`text is ${text}`)
-            return [...curVal, {sender: loggedInUser, text}] 
-        })
+    inputRef.current.addEventListener('keyup', onEnterPressed)
+    return () => {
+      if (!inputRef.current) return
+      inputRef.current.removeEventListener('keyup', onEnterPressed)
     }
-    useEffect(()=>{
-        if(!inputRef) return;
-        const onEnterPressed = (e) => {
-            console.log(e)
-            if (e.keyCode === 13) {
-                e.preventDefault();
-                updateMessgages(e.target.defaultValue) 
-                clearnUpInput()
-            }
-        }
-        inputRef.addEventListener("keyup", onEnterPressed)
-    },[inputRef])
-    console.log(text)
-    return(
-        <div style={{height: "50px", width: "100%", display: "flex", position: "absolute", bottom: 0, right: 0}}>
-            <input value={text} 
-                onChange={(e)=>changeText(e.target.value)} style={{flex: 3}} ref={setInputRef}/> 
-            <button style={{flex: 1}} onClick={()=> {
-                updateMessgages(text)
-                clearnUpInput()
-            }}>send</button>
-        </div>
-    )
+  }, [text])
+  return (
+    <InputGroup>
+      <Input
+        placeholder="your message..."
+        value={text}
+        onChange={(e) => changeText(e.target.value)}
+        style={{ flex: 3 }}
+        ref={inputRef}
+      />
+      <InputRightElement width="4.5rem">
+        <Button
+          h="1.75rem"
+          size="sm"
+          onClick={() => {
+            updateMessgages(text)
+            portal.emitSendMessage(loggedInUser, text)
+            clearnUpInput()
+          }}
+        >
+          send
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  )
 }
