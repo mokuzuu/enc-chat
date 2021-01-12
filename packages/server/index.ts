d9ca96e0d1b6ce060f701f79d20e6630e8eed73f
Taiga <mokuzuzzz@gmail.com>
2021-01-13 03:07:03 +1000

    feat: add server to listent to socket request

---
+++ packages/server/index.ts
@@ -0,0 +1,34 @@
+import { Socket } from 'socket.io'
+
+const app = require('express')()
+const http = require('http').Server(app)
+
+const io = require('socket.io')(http, {
+  origin: 'http://localhost:3000',
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+  },
+})
+const cors = require('cors')
+
+app.use(cors())
+
+const PORT = 8000
+io.on('connection', (socket: Socket) => {
+  console.log('CONNECTED')
+  socket.on('join-room', (roomId, user) => {
+    socket.to(roomId).emit('join-room', user)
+  })
+  socket.on('send-message', (roomId, sender, message) => {
+    console.log(`encrypted sender: ${sender}`)
+    console.log(`encrypted message: ${message}`)
+    socket.to(roomId).emit('new-message', sender, message)
+  })
+})
+
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`)
+})
