ea8f79cb97f972fde531b775b5bb58395b2d5f45
Taiga <mokuzuzzz@gmail.com>
2021-01-13 03:14:52 +1000

    feat: add message

--- packages/app/components/message.tsx
+++ packages/app/components/message.tsx
@@ -1,30 +1,40 @@
 export const Message = ({
-    sender: name,
-    text
+  sender: name,
+  text,
 }: {
-    sender: string
-    text: string
+  sender: string
+  text: string
 }) => {
-    return (
-        <div style={{
-            minHeight: "100px",
-            width: "100%",
-            borderBottom: "1px solid grey",
-            display: "flex"
-        }}>
-            <div style={{
-                flexBasis: "100px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                borderRight: "1px solid grey"
-            }}>{name}</div>
-            <div style={{
-                flex: 1,
-                display: "flex",
-                alignItems: "center",
-                paddingLeft: "10px"
-            }}>{text}</div>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        minHeight: '100px',
+        width: '100%',
+        borderBottom: '1px solid grey',
+        display: 'flex',
+      }}
+    >
+      <div
+        style={{
+          flexBasis: '100px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRight: '1px solid grey',
+        }}
+      >
+        {name}
+      </div>
+      <div
+        style={{
+          flex: 1,
+          display: 'flex',
+          alignItems: 'center',
+          paddingLeft: '10px',
+        }}
+      >
+        {text}
+      </div>
+    </div>
+  )
 }
